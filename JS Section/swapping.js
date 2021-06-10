97332e2d25c0335aec313e2adc4906e0188bfa8f
ANSHID-P <anshianshid123@gmail.com>
2021-06-10 11:09:50 +0530

    js first class

---
+++ JS Section/swapping.js
@@ -0,0 +1,14 @@
+var num1=10, num2=20; num3=0; num4=0;
+console.log(`values b4 swapping num1=${num1} num2=${num2}`);
+
+num3=num1;
+num4=num2;
+num1=num4;
+num2=num3;
+
+
+// num3=num1;
+// num4=num2;
+
+console.log(`values b4 swapping num1=${num1} num2=${num2}`);
+
