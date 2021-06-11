e0f29ddfe5adde839c4a6e04699438745b82f846
ANSHID-P <anshianshid123@gmail.com>
2021-06-11 11:01:34 +0530

    11june js

---
+++ JS Section/decuition making/largnum.js
@@ -0,0 +1,15 @@
+var num1,num2, num3;
+
+
+if(num1>num2 & num1>num3){
+    console.log(`${num1} is largest`);
+}
+else if(num2>num1 & num2>num3){
+    console.log(`${num2} is largest`);
+}
+if(num3>num1 & num3>num2){
+    console.log(`${num3} is largest`);
+}
+else if(num1==num2 & num1==num3){
+    console.log("all these equal");
+}
